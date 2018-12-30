171eb28dc87da38d601669707a4f42c1b94c6116
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-12-30 05:50:22 +0530

    feat(listing): fetch user data and pass it as props to UserTable

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
+
 import { http } from '../../utils/httpModule'
 import { logUrlError } from '../../utils/errorLogs'
+import { USERS_URL } from '../../utils/constants'
+import UserTable from './UserTable'
 
 class Listing extends Component {
+  state = {
+    users: [],
+  }
+
   componentDidMount() {
     http
-      .GET('')
-      .map(res => res.data)
-      .fork(logUrlError, console.log)
+      .GET(USERS_URL)
+      .map(res => res.data.users)
+      .fork(logUrlError, users => this.setState({ users }))
   }
 
   render() {
-    return <span>Listing</span>
+    const { users } = this.state
+    return (
+      <Grid container spacing={8}>
+        <Grid item xs={12}>
+          <UserTable users={users} />
+        </Grid>
+      </Grid>
+    )
   }
 }
 
