50ab98ed605e68546c55acb70ea70b25b36356af
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-12-30 05:48:48 +0530

    chore(constants): break the base url and create a user url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-const BASE_URL = 'https://api.myjson.com/bins/pkisp/'
+const BASE_URL = 'https://api.myjson.com/bins/'
+const USERS_URL = 'pkisp/'
 
-export { BASE_URL }
+export { BASE_URL, USERS_URL }
